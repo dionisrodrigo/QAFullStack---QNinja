1606cd168fd4b8caded9936b697abafe735ad3c6
Dionis Moreira <dionis.rodrigo@gmail.com>
2019-08-16 12:55:01 -0300

    Criando o arquivo de Metodo

--- Metodo.rb
+++ Metodo.rb
@@ -4,5 +4,22 @@ end
 
 ola
 
-def method_name
+def retornar_nome
+  "Dionis Rodrigo Moreira"
 end
+
+resultado = retornar_nome
+puts resultado
+
+def soma(v1, v2)
+  total = v1 + v2
+  puts total
+end
+
+res = soma(15, 5)
+
+def preencherNome(nome)
+  puts "Ola " + nome
+end
+
+preencherNome("Dionis Moreira")
